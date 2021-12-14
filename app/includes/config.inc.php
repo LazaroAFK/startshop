b41366059ea9ad1a991c32fd40fc95ec2e329ace
Lazaro Contreras <Lazaro.Contreras@hotmail.com>
2021-12-14 15:48:03 -0700

    reconfigure

--- app/includes/config.inc.php
+++ app/includes/config.inc.php
@@ -9,7 +9,7 @@ define('DBPASSWORD', '');
 // Apoyo para acceso publicos y privados
 
 // Publica (virtual host)
-define('URLROOT', 'https://startshop.amigable.dev/');
+define('URLROOT', 'https://startshop.amigable.dev');
 
 // Privada
 define('APPROOT', dirname(dirname(__FILE__)));
