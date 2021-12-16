16c874fff248ed0b3daf69654611201b52defeef
Lazaro Contreras <Lazaro.Contreras@hotmail.com>
2021-12-15 17:03:55 -0700

    test

--- app/views/index.php
+++ app/views/index.php
@@ -8,35 +8,36 @@ console($data);
 
 include_once(APPROOT . '/views/includes/header.inc.php');
 ?>
-
-<div class="flex-none flex flex-nowrap gap-2" style="max-width: 685px">
-  <a href="#" class="flex-none h-8 px-3 rounded bg-gray-200 flex items-center justify-center">
-    <span class="font-medium">Productos</span>
-  </a>
-  <a href="#" class="flex-none h-8 px-3 flex items-center justify-center">
-    <span class="font-medium">Inventarios</span>
-  </a>
-  <div class="flex-grow h-8 flex justify-end">
-    <a href="#" title="Ayuda" class="w-8 h-8 rounded hover:bg-gray-200 flex items-center justify-center">
-      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-help" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <circle cx="12" cy="12" r="9"></circle>
-        <line x1="12" y1="17" x2="12" y2="17.01"></line>
-        <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4"></path>
-      </svg>
-    </a>
-  </div>
-</div>
-<div class="flex-none mb-10 flex gap-20 justify-center">
-    <div class="w-96 h-96 bg-red-300">
-        <canvas id="myChart1" width="400" height="400"></canvas>
+<div class="flex-grow p-20 overflow-y-scroll flex flex-col gap-4">
+    <div class="flex-none flex flex-nowrap gap-2" style="max-width: 685px">
+        <a href="#" class="flex-none h-8 px-3 rounded bg-gray-200 flex items-center justify-center">
+            <span class="font-medium">Gráficas</span>
+        </a>
+        <div class="flex-grow h-8 flex justify-end">
+            <a href="#" title="Ayuda" class="w-8 h-8 rounded hover:bg-gray-200 flex items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-help" width="24" height="24"
+                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                    stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <circle cx="12" cy="12" r="9"></circle>
+                    <line x1="12" y1="17" x2="12" y2="17.01"></line>
+                    <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4"></path>
+                </svg>
+            </a>
+        </div>
     </div>
-    <div class="w-96 h-96 bg-red-300">
-        <canvas id="myChart2" width="400" height="400"></canvas>
+    <div class="flex-none mb-10 flex gap-20 justify-center">
+        <label for="" class="flex-shrink w-72 p-px font-medium">Productos</label>
+        <div class="w-96 h-96 bg-red-300">
+            <canvas id="myChart1" width="400" height="400"></canvas>
+        </div>
     </div>
-</div>
-<div class="flex-none mb-10 flex gap-3">
-  <div class="w-full h-96 bg-red-300"></div>
+    <div class="flex-none mb-10 flex gap-3">
+        <label for="" class="flex-shrink w-72 p-px font-medium">Inventarios</label>
+        <div class="w-96 h-96 bg-red-300">
+            <canvas id="myChart2" width="400" height="400"></canvas>
+        </div>
+    </div>
 </div>
 
 <script src="<?= URLROOT; ?>/js/chart.min.js"></script>
