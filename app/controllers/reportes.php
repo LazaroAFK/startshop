9a33b2be6656e1f205141436f7a85717fa8c12c8
Lazaro Contreras <Lazaro.Contreras@hotmail.com>
2021-12-15 18:25:29 -0700

    test

--- app/controllers/reportes.php
+++ app/controllers/reportes.php
@@ -3,7 +3,7 @@
 class reportes extends controller{
     
     public function __construct(){
-        $this -> reporteModel = $this -> model('reporte');
+        // $this -> reporteModel = $this -> model('reporte');
     }
 
     public function index(){
