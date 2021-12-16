c1cb126dc26f6941a91b7bcb27002aede0d02d4e
Lazaro Contreras <Lazaro.Contreras@hotmail.com>
2021-12-15 17:45:35 -0700

    test

--- app/views/index.php
+++ app/views/index.php
@@ -88,6 +88,7 @@ const myChart1 = new Chart(ctx1, {
         }]
     },
     options: {
+        responsive: true,
         scales: {
             y: {
                 beginAtZero: true
@@ -125,25 +126,26 @@ const myChart2 = new Chart(ctx2, {
             label: 'IVA Proveedores',
             data: [<?= $proveedoresIVA ?>],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
             ],
             borderWidth: 1
         }]
     },
     options: {
+        responsive: true,
         scales: {
             y: {
                 beginAtZero: true
