bd06390e822d8f717bf5d6616a3e1e0f84d5c046
Lazaro Contreras <Lazaro.Contreras@hotmail.com>
2021-12-15 18:24:33 -0700

    test

---
+++ app/controllers/reportes.php
@@ -0,0 +1,12 @@
+<?php
+
+class reportes extends controller{
+    
+    public function __construct(){
+        $this -> reporteModel = $this -> model('reporte');
+    }
+
+    public function index(){
+        $this -> view('/construccion');
+    }
+}
