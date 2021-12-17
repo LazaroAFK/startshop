8bd89fcb03acff7d2639dd78f2ec4e63bfcf36f4
Lazaro Contreras <Lazaro.Contreras@hotmail.com>
2021-12-16 17:18:37 -0700

    test

--- app/controllers/ventas.php
+++ app/controllers/ventas.php
@@ -10,15 +10,16 @@ class ventas extends controller{
     public function index(){
         $escaner = [];
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $arreglo = $_SESSION['lista_venta'];
             $escaner = $this -> ventaModel -> buscar($_POST['codigo']);
-            $escaner = array_merge($_SESSION['lista_venta'], $escaner);
-            $_SESSION['lista_venta'] = $escaner
+            $arreglo = array_push($arreglo, $escaner);
+            $_SESSION['lista_venta'] = $arreglo
         }
 
-        $this -> view('ventas/index', $escaner);
+        $this -> view('ventas/index', $arreglo);
     }
 
     public function cobrar(){
-        // $_SESSION['lista_venta'] = [];
+        $_SESSION['lista_venta'] = [];
     }
 }
