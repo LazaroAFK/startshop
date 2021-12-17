32262745e7627406bec09705b97e3162811b8ca3
Lazaro Contreras <Lazaro.Contreras@hotmail.com>
2021-12-16 17:09:43 -0700

    test

--- app/controllers/ventas.php
+++ app/controllers/ventas.php
@@ -3,21 +3,22 @@
 // Carga inicial
 class ventas extends controller{
     public function __construct(){
-        $_SESSION['lista_venta'] = [];
+        // $_SESSION['lista_venta'] = [];
         $this -> ventaModel = $this -> model('venta');
     }
     
     public function index(){
+        $escaner = [];
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
             $escaner = $this -> ventaModel -> buscar($_POST['codigo']);
             // $escaner = array_merge($_SESSION['lista_venta'], $escaner);
-            $_SESSION['lista_venta'] = $escaner
+            // $_SESSION['lista_venta'] = $escaner
         }
 
         $this -> view('ventas/index', $escaner);
     }
 
     public function cobrar(){
-        $_SESSION['lista_venta'] = [];
+        // $_SESSION['lista_venta'] = [];
     }
 }
