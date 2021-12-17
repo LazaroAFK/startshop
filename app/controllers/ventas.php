1e578b75fb0c6db9d2e68235708eb4d24ffa11bd
Lazaro Contreras <Lazaro.Contreras@hotmail.com>
2021-12-16 17:47:02 -0700

    test

--- app/controllers/ventas.php
+++ app/controllers/ventas.php
@@ -9,10 +9,14 @@ class ventas extends controller{
     
     public function index(){
         $arreglo = $_SESSION['lista_venta'];
+        console($arreglo);
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
             $escaner = $this -> ventaModel -> buscar($_POST['codigo']);
+            console($escaner);
             $arreglo = array_push($escaner);
+            console($arreglo);
             $_SESSION['lista_venta'] = $arreglo;
+            console($arreglo);
         }
 
         $this -> view('ventas/index', $arreglo);
